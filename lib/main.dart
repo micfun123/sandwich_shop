4b6de2ce9cea6d6a3adcedaf97a9bd66628ccfc8
Michael Parker <michaelrbparker@protonmail.com>
2025-10-24 14:45:49 +0100

    row, coloums

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const App());
 }
+
 class App extends StatelessWidget {
   const App({super.key});
 
@@ -12,15 +13,58 @@ class App extends StatelessWidget {
       title: 'Sandwich Shop App',
       home: Scaffold(
         appBar: AppBar(title: const Text('Sandwich Counter')),
-        body: const Center(
-          child: OrderItemDisplay(5, 'Footlong'),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(16.0),
+                    child: const OrderItemDisplay(3, 'Turkey'),
+                    color: Colors.amber[600],
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(16.0),
+                    child: const OrderItemDisplay(2, 'Footlong'),
+                    color: const Color.fromARGB(255, 0, 17, 255),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(16.0),
+                    child: const OrderItemDisplay(5, 'Footlong'),
+                    color: const Color.fromARGB(255, 66, 97, 10),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(16.0),
+                    color: const Color.fromARGB(255, 255, 0, 191),
+                    child: const OrderItemDisplay(9, 'Turkey'),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(16.0),
+                    child: const OrderItemDisplay(4, 'Footlong'),
+                    color: const Color.fromARGB(255, 255, 0, 0),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(16.0),
+                    child: const OrderItemDisplay(1, 'Footlong'),
+                    color: const Color.fromARGB(255, 30, 122, 122),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
-
 }
-
 
 class OrderItemDisplay extends StatelessWidget {
   final String itemType;
@@ -30,7 +74,12 @@ class OrderItemDisplay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('$quantity $itemType sandwich(es): ${'🥪' * quantity}');
+    return Text(
+      '$quantity $itemType sandwich(es): ${'🥪' * quantity}',
+      style: TextStyle(
+        fontSize: 20,
+        color: Colors.white,
+      ),
+    );
   }
-
 }
