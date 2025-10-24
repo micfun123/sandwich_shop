bf1cdb66bdf7d9eabe94b48bd2dc021410acd7ac
Michael Parker <michaelrbparker@protonmail.com>
2025-10-24 14:01:28 +0100

    Use OrderItemDisplay in App

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,31 @@ void main() {
 class App extends StatelessWidget {
   const App({super.key});
 
- @override
-    Widget build(BuildContext context) {
-      return MaterialApp(
-        title: 'Sandwich Shop App',
-        home: Scaffold(
-          appBar: AppBar(title: const Text('Sandwich Counter')),
-          body: const Center(
-            child: Text('Welcome to the Sandwich Shop!'),
-          ),
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sandwich Shop App',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Sandwich Counter')),
+        body: const Center(
+          child: OrderItemDisplay(5, 'Footlong'),
         ),
-      );
-    }
+      ),
+    );
+  }
+
 }
 
+
+class OrderItemDisplay extends StatelessWidget {
+  final String itemType;
+  final int quantity;
+
+  const OrderItemDisplay(this.quantity, this.itemType, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('$quantity $itemType sandwich(es): ${'🥪' * quantity}');
+  }
+
+}
