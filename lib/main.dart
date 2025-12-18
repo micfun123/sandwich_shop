7b813b1150b47f1e03c7d9ff1d27d35d89f998f4
Michael Parker <michaelrbparker@protonmail.com>
2025-12-18 21:58:26 +0000

    Initialize database for desktop platforms in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:sandwich_shop/models/cart.dart';
 import 'package:sandwich_shop/views/order_screen.dart';
 import 'package:sandwich_shop/views/app_styles.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+  }
+
   await AppStyles.loadFontSize();
   runApp(const App());
 }
